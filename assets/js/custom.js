941e42ecd635d644f730d928ce51c4d5269b3e79
Elise van der Pol <koster.elise@gmail.com>
2014-02-12 11:40:16 +0100

    append fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,4 @@
 function formSubmit(){
 	console.log("entered");
- 	$('#form1').append("<br><div id='confirm'>Je reactie is ontvangen!</div>");
+ 	$(this).append("<br><div id='confirm'>Je reactie is ontvangen!</div>");
 }
