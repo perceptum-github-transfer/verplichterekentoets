c6dcf2c076b42dc28ac84347b0a8661a67f2e094
Elise van der Pol <koster.elise@gmail.com>
2014-02-12 12:25:56 +0100

    still no feedback on the bottom form

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,16 +1,14 @@
-if(window.jQuery){
-	console.log("ya");
-}
-else{
-	console.log("no")
-}
+
 
 function formSubmit(){
 	console.log("entered");
- 	$("#form1").append("<br><div id='confirm'>Je reactie is ontvangen!</div>");
+	document.getElementById("form1").appendChild("<br><div id='confirm'>Je reactie is ontvangen!</div>");
+
+ 	//$("#form1").append("<br><div id='confirm'>Je reactie is ontvangen!</div>");
 }
 
 function formSubmitDown(){
 	console.log("entered2");
-	$("#form2").append("<br><div id='confirmDown'>Je reactie is ontvangen!</div>");
+	document.getElementById("form2").appendChild("<br><div id='confirm'>Je reactie is ontvangen!</div>");
+	//$("#form2").append("<br><div id='confirmDown'>Je reactie is ontvangen!</div>");
 }
