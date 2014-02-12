bad1bc7ec2e3e252332828e531ff316b12cf2aa7
Elise van der Pol <koster.elise@gmail.com>
2014-02-12 11:56:02 +0100

    another typo

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,9 @@
 function formSubmit(){
 	console.log("entered");
- 	$(#form1).append("<br><div id='confirm'>Je reactie is ontvangen!</div>");
+ 	$("#form1").append("<br><div id='confirm'>Je reactie is ontvangen!</div>");
 }
 
 function formSubmitDown(){
 	console.log("entered2");
-	$(#form2).append("<br><div id='confirm'>Je reactie is ontvangen!</div>");
+	$("#form2").append("<br><div id='confirm'>Je reactie is ontvangen!</div>");
 }
